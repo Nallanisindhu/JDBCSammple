a4c06094d20b31527bc493d8099b5d38eaa05464
Nallanisindhu <77882874+Nallanisindhu@users.noreply.github.com>
2021-03-11 14:27:42 +0530

    test

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -36,7 +36,8 @@ public class MainClass {
 			System.out.println("age greaterthan 25");
 		List l=	c.stream().filter((x)->x.Age<=30).collect(Collectors.toList());
 		
-		l.forEach((Y)->{
+		l.forEach((Y)->
+		{
 			System.out.println(Y);
 		});
 		con.close();		
